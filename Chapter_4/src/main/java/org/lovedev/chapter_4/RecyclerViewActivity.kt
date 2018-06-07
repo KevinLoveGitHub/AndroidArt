27a90f54d99e9d45fc4199328f6b73c5c940d24f
Kevin <616651408@qq.com>
2018-06-07 15:50:18 +0800

    Add kotlin demo

---
+++ Chapter_4/src/main/java/org/lovedev/chapter_4/RecyclerViewActivity.kt
@@ -0,0 +1,33 @@
+package org.lovedev.chapter_4
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+class RecyclerViewActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+
+        var myDataset = arrayOf("123", "123", "123", "123", "123")
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = MyAdapter(myDataset)
+
+        recyclerView = findViewById<RecyclerView>(R.id.rv).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
+    }
+}
---
+++ Chapter_4/src/main/java/org/lovedev/chapter_4/Test.kt
@@ -0,0 +1,9 @@
+package org.lovedev.chapter_4
+
+/**
+ * @author Kevin
+ * @data 2018/6/7
+ */
+class Test (name: String) {
+
+}
